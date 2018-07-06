7c8ecbdfb2c136789459b2320196e2c2c853bdbe
tomergal <tomer1404@walla.com>
2018-07-06 14:58:06 +0300

    navbar component

--- EntityGenerator/src/app/app.module.ts
+++ EntityGenerator/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
